f7d370124fcd82f0df5e4114bb1b7a26b96d0ae2
Fred Morcos <fred.morcos@open-xchange.com>
2023-04-07 17:26:09 +0200

    Only print clang-tidy messages from files in the compdb

--- .github/scripts/clang-tidy.py
+++ .github/scripts/clang-tidy.py
@@ -8,8 +8,10 @@ suggested fixes to Github Actions annotations.
 """
 
 import argparse
+import json
 import os
 import sys
+from pathlib import Path
 
 import yaml
 
@@ -52,9 +54,29 @@ def load_file(filename):
         return contents
 
 
+def load_compdb(fixes_filename):
+    """Load the compilation database next to the fixes file."""
+    fixes_path = Path(fixes_filename)
+    compdb_filename = os.path.join(fixes_path.parent, "compile_commands.json")
+    with open(compdb_filename, encoding="utf_8") as file:
+        return json.load(file)
+
+
+def index_compdb(compdb):
+    """Index the compilation database."""
+    result = set()
+    for item in compdb:
+        filename = os.path.join(item["directory"], item["file"])
+        result.add(filename)
+    return result
+
+
 def main():
     """Start the script."""
     args = create_argument_parser()
+
+    compdb = load_compdb(args.fixes_file)
+    compdb = index_compdb(compdb)
 
     fixes = load_fixes_file(args.fixes_file)
     fixes = fixes["Diagnostics"]
@@ -62,20 +84,38 @@ def main():
     for fix in fixes:
         name = fix["DiagnosticName"]
         level = fix["Level"]
-        # directory = fix["BuildDirectory"]
+        directory = fix["BuildDirectory"]
         diagnostic = fix["DiagnosticMessage"]
         offset = diagnostic["FileOffset"]
         filename = diagnostic["FilePath"]
         message = diagnostic["Message"]
 
-        # full_filename = directory + os.path.sep + filename
+        if filename == "":
+            print(f"Meta error message from `{directory}`: {message}")
+            continue
+
         full_filename = filename
+        full_filename = Path(full_filename)
+        full_filename = (
+            full_filename.as_posix()
+            if full_filename.is_absolute()
+            else os.path.join(directory, filename)
+        )
+
+        if full_filename not in compdb:
+            print(
+                f"Skipping `{full_filename}`"
+                " because it is not found"
+                " in the compilation database"
+            )
+            continue
+
         try:
             file_contents = load_file(full_filename)
         except OSError:
             # Skip in case the file can't be found. This is usually one of
             # those "too many errors emitted, stopping now" clang messages.
-            print(f"Skipping {full_filename}")
+            print(f"Skipping `{full_filename}` because it is not found")
             continue
 
         line = get_line_from_offset(file_contents, offset)
